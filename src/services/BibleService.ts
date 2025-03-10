a2c41583549869260697e4d41bcfa66531549fb7
Scott Tomaszewski <scott.tomaszewski@finxact.com>
2025-03-09 20:52:05 -0400

    cleanup

--- src/services/BibleService.ts
+++ src/services/BibleService.ts
@@ -33,7 +33,35 @@ export class BibleService {
         this.addBookMapping("Ruth", ["Rth", "Ru"]);
         this.addBookMapping("1 Samuel", ["1 Sam", "1 Sa", "1S", "I Sa", "1Sam", "1st Samuel"]);
         this.addBookMapping("2 Samuel", ["2 Sam", "2 Sa", "2S", "II Sa", "2Sam", "2nd Samuel"]);
-        // ... Add more books as needed
+        this.addBookMapping("1 Kings", ["1 Ki", "1 K", "1K", "I K", "1Ki", "1st Kings"]);
+        this.addBookMapping("2 Kings", ["2 Ki", "2 K", "2K", "II K", "2Ki", "2nd Kings"]);
+        this.addBookMapping("1 Chronicles", ["1 Ch", "1 Chr", "I Ch", "1Ch", "1st Chronicles"]);
+        this.addBookMapping("2 Chronicles", ["2 Ch", "2 Chr", "II Ch", "2Ch", "2nd Chronicles"]);
+        this.addBookMapping("Ezra", ["Ezr", "Ez"]);
+        this.addBookMapping("Nehemiah", ["Neh", "Ne"]);
+        this.addBookMapping("Esther", ["Est", "Es"]);
+        this.addBookMapping("Job", ["Jb"]);
+        this.addBookMapping("Psalms", ["Ps", "Psa", "Psalm", "Pslm"]);
+        this.addBookMapping("Proverbs", ["Prov", "Pro", "Pr"]);
+        this.addBookMapping("Ecclesiastes", ["Eccl", "Ecc", "Ec", "Qoh"]);
+        this.addBookMapping("Song of Solomon", ["Song", "SoS", "Canticles", "Song of Songs", "Cant"]);
+        this.addBookMapping("Isaiah", ["Isa", "Is"]);
+        this.addBookMapping("Jeremiah", ["Jer", "Je"]);
+        this.addBookMapping("Lamentations", ["Lam", "La"]);
+        this.addBookMapping("Ezekiel", ["Ezek", "Eze", "Ezk"]);
+        this.addBookMapping("Daniel", ["Dan", "Da", "Dn"]);
+        this.addBookMapping("Hosea", ["Hos", "Ho"]);
+        this.addBookMapping("Joel", ["Jl"]);
+        this.addBookMapping("Amos", ["Am"]);
+        this.addBookMapping("Obadiah", ["Obad", "Ob"]);
+        this.addBookMapping("Jonah", ["Jon", "Jnh"]);
+        this.addBookMapping("Micah", ["Mic", "Mi"]);
+        this.addBookMapping("Nahum", ["Nah", "Na"]);
+        this.addBookMapping("Habakkuk", ["Hab", "Hb"]);
+        this.addBookMapping("Zephaniah", ["Zeph", "Zep", "Zp"]);
+        this.addBookMapping("Haggai", ["Hag", "Hg"]);
+        this.addBookMapping("Zechariah", ["Zech", "Zec", "Zc"]);
+        this.addBookMapping("Malachi", ["Mal", "Ml"]);
         
         // New Testament
         this.addBookMapping("Matthew", ["Matt", "Mt"]);
@@ -44,7 +72,25 @@ export class BibleService {
         this.addBookMapping("Romans", ["Rom", "Ro", "Rm"]);
         this.addBookMapping("1 Corinthians", ["1 Cor", "1 Co", "I Co", "1Cor", "1st Corinthians"]);
         this.addBookMapping("2 Corinthians", ["2 Cor", "2 Co", "II Co", "2Cor", "2nd Corinthians"]);
-        // ... Add more books as needed
+        this.addBookMapping("Galatians", ["Gal", "Ga"]);
+        this.addBookMapping("Ephesians", ["Eph", "Ep"]);
+        this.addBookMapping("Philippians", ["Phil", "Php", "Pp"]);
+        this.addBookMapping("Colossians", ["Col", "Co"]);
+        this.addBookMapping("1 Thessalonians", ["1 Thess", "1 Th", "I Th", "1Thess", "1st Thessalonians"]);
+        this.addBookMapping("2 Thessalonians", ["2 Thess", "2 Th", "II Th", "2Thess", "2nd Thessalonians"]);
+        this.addBookMapping("1 Timothy", ["1 Tim", "1 Ti", "I Ti", "1Tim", "1st Timothy"]);
+        this.addBookMapping("2 Timothy", ["2 Tim", "2 Ti", "II Ti", "2Tim", "2nd Timothy"]);
+        this.addBookMapping("Titus", ["Tit", "Ti"]);
+        this.addBookMapping("Philemon", ["Phlm", "Phm", "Pm"]);
+        this.addBookMapping("Hebrews", ["Heb", "He"]);
+        this.addBookMapping("James", ["Jas", "Jm"]);
+        this.addBookMapping("1 Peter", ["1 Pet", "1 Pe", "I Pe", "1Pet", "1st Peter"]);
+        this.addBookMapping("2 Peter", ["2 Pet", "2 Pe", "II Pe", "2Pet", "2nd Peter"]);
+        this.addBookMapping("1 John", ["1 Jn", "I Jn", "1Jn", "1st John"]);
+        this.addBookMapping("2 John", ["2 Jn", "II Jn", "2Jn", "2nd John"]);
+        this.addBookMapping("3 John", ["3 Jn", "III Jn", "3Jn", "3rd John"]);
+        this.addBookMapping("Jude", ["Jud", "Jd"]);
+        this.addBookMapping("Revelation", ["Rev", "Re", "The Revelation", "Apocalypse"]);
     }
 
     /**
